d86c51aa06787116238879519b4f050a43e6114f
morganmmoore <morganmmoore0@gmail.com>
2021-12-12 20:11:56 -0500

    Created server.js and workout.js model

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter a workout name"
+    },
+    type: {
+        type: String,
+        required: "Enter workout type"
+    },
+    duration: {
+        type: Number,
+        required: "Enter workout duration"
+    },
+    distance: {
+        tytpe: Number,
+    },
+    sets: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    weights: {
+        type: Number
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
